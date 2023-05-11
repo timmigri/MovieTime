e19533252f318c05425119786426f9588089c712
Artem <timmi.gri@gmail.com>
2023-05-11 20:30:30 +0300

    Initial Commit

---
+++ MovieTime/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieTime
+//
+//  Created by Артём Грищенко on 11.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieTime/MovieTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTimeApp.swift
+//  MovieTime
+//
+//  Created by Артём Грищенко on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
