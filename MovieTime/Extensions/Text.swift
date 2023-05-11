c8170cc4db8646c2288e51d9cf49f0926e837f64
Artem <timmi.gri@gmail.com>
2023-05-12 02:47:35 +0300

    add actor section ui in search page

--- MovieTime/Extensions/Text.swift
+++ MovieTime/Extensions/Text.swift
@@ -9,10 +9,22 @@ import Foundation
 import SwiftUI
 
 extension Text {
+    func caption2() -> some View {
+        return self
+            .font(.sf(.regular, size: 12))
+            .lineSpacing(18 - 12)
+    }
+    
     func heading2() -> some View {
         return self
             .font(.sf(.bold, size: 24))
             .lineSpacing(32 - 24)
+    }
+    
+    func bodyText3() -> some View {
+        return self
+            .font(.sf(.bold, size: 18))
+            .lineSpacing(24 - 18)
     }
 
     func bodyText5() -> some View {
--- MovieTime/Views/SearchScreenView.swift
+++ MovieTime/Views/SearchScreenView.swift
@@ -23,19 +23,60 @@ struct SearchScreenView: View {
                     TextField("Search for Movies, Series", text: $query) {
                         searchFieldFocused = $0
                     }
-                        .frame(height: 44)
-                        .accentColor(.appPrimary)
-                        .foregroundColor(.appSecondary300)
+                    .frame(height: 44)
+                    .accentColor(.appPrimary)
+                    .foregroundColor(.appSecondary300)
+                
+                    Image("FilterIcon")
+                        .padding(.leading, 10)
+                        .padding(.vertical, 10)
+                        .animation(.easeInOut, value: 5)
                         .padding(.trailing, 10)
+                    
                 }
                 .overlay(RoundedRectangle(cornerRadius: 5)
                     .stroke(searchFieldFocused ? Color.appPrimary : Color.appSecondary300, lineWidth: 1))
-//                ScrollView {
-//
-//                }.frame(maxHeight: .infinity)
-                noInputView
+                ScrollView {
+                    actorsSection
+                }
+                .padding(.top, 32)
+                .frame(maxHeight: .infinity)
             }
             .padding()
+        }
+    }
+    
+    var actorsSection: some View {
+        VStack(alignment: .leading) {
+            Text("Actors")
+                .bodyText3()
+                .foregroundColor(.appTextWhite)
+            GeometryReader { geometry in
+                HStack(alignment: .top, spacing: 0) {
+                    testActor(geometry)
+                    testActor(geometry)
+                    testActor(geometry)
+                    testActor(geometry)
+                    testActor(geometry)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    func testActor(_ geometry: GeometryProxy) -> some View {
+        let padding: CGFloat = 10
+        let width = (geometry.size.width - padding * 4) / 4
+        print(width, geometry.size.width)
+        return VStack {
+            Image("ActorExample")
+                .frame(width: width, height: width)
+            Text("Dwayne Johnson")
+                .caption2()
+                .fixedSize(horizontal: false, vertical: true)
+                .lineLimit(2)
+                .foregroundColor(.appSecondary300)
+                .multilineTextAlignment(.center)
         }
     }
 
@@ -49,6 +90,24 @@ struct SearchScreenView: View {
                 .foregroundColor(.appTextWhite)
                 .padding(.bottom, 10)
             Text("By typing in search box, MovieTime search in movies, series and actors then show you the best results.")
+                .bodyText5()
+                .multilineTextAlignment(.center)
+                .foregroundColor(.appTextBlack)
+                .padding(.bottom, 10)
+        }
+        .frame(maxHeight: .infinity)
+    }
+    
+    var noResultView: some View {
+        VStack {
+            Image("NoResultPicture")
+                .padding(.bottom, 25)
+            Text("No result")
+                .heading2()
+                .multilineTextAlignment(.center)
+                .foregroundColor(.appTextWhite)
+                .padding(.bottom, 10)
+            Text("No results found, Please try other words")
                 .bodyText5()
                 .multilineTextAlignment(.center)
                 .foregroundColor(.appTextBlack)
